c29ff13d23b4d71234164786089a46ec5fc7200e
Paulo Campez <paulo.campez.magit@outlook.com>
2020-03-02 16:37:36 -0300

    First Commit
    First Commit

---
+++ ProCsGo/ProCsGo/ProCsGo.Domain/Service/UserServiceAsync.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using AutoMapper;
+using ProCsGo.Entity;
+using ProCsGo.Entity.UnitofWork;
+
+namespace ProCsGo.Domain.Service
+{
+
+    public class UserServiceAsync<Tv, Te> : GenericServiceAsync<Tv, Te>
+                                                where Tv : UserViewModel
+                                                where Te : User
+    {
+        //DI must be implemented specific service as well beside GenericAsyncService constructor
+        public UserServiceAsync(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            if (_unitOfWork == null)
+                _unitOfWork = unitOfWork;
+            if (_mapper == null)
+                _mapper = mapper;
+        }
+
+        //add here any custom service method or override genericasync service method
+        //...
+    }
+
+}
